ce157dd40639d2b0fa1bc5bf6634c53e44d2702a
mortazavi <s.mohammad027@live.com>
2023-05-28 15:25:22 +0330

    methods extracted;

--- src/Services/AsheiService.php
+++ src/Services/AsheiService.php
@@ -2,6 +2,7 @@
 
 	namespace Hans\Ashei\Services;
 
+	use Epubli\Epub\Data\Item;
 	use Epubli\Epub\Epub;
 	use Epubli\Exception\Exception;
 	use Illuminate\Support\Str;
@@ -12,54 +13,66 @@
 		 * @throws Exception
 		 */
 		public function read( string $book ): array {
-
 			// setting up
 			$epub    = new Epub( $book );
 			$content = null;
 			foreach ( $epub->getSpine() as $index => $spine ) {
 				// find titles
-				$matches   = [];
-				$ifMatched = preg_match_all( '/(<h[1-4].*>.*<\/h[1-4]>)+/', $spine->getData(), $matches );
-				$titles    = [];
-				if ( $ifMatched ) {
-					foreach ( $matches as $match ) {
-						foreach ( $match as $item ) {
-							$title  = [];
-							$titled = preg_match_all( '/\>(.+)<\/+/', $item, $title );
-							if ( $titled ) {
-								$titles[] = Str::remove( [ "/>", "</" ], $title[ 1 ][ 0 ] );
-							}
-						}
-					}
-				}
+				$titles = $this->extractTitles( $spine );
 				// parse contents
-				$text = $spine->getContents();
-				$text = Str::replace( [ "\t\t\t\n", "\t\t\n", "\t\t\t", "\t" ], '', $text );
-				$text = preg_split( '/(\\n)+/', $text );
-				if ( is_array( $text ) ) {
-					foreach ( $text as $key => $item ) {
-						if ( Str::wordCount( $item ) == 0 ) {
-							unset( $text[ $key ] );
-							continue;
-						}
-						// wrap titles with title tag
-						if ( in_array( $item, $titles ) ) {
-							$text[ $key ] = "<h3>" . $item . "</h3>";
-						}
-					}
-					if ( empty( $text ) ) {
-						continue;
-					}
-				} elseif ( is_string( $text ) ) {
-					if ( Str::wordCount( $text ) == 0 ) {
-						continue;
-					}
+				$text = $this->extractText( $spine, $titles );
+				if ( empty( $text ) ) {
+					continue;
 				}
 				$content[ $index ] = $text;
 				$spine->close();
 			}
 
 			// put together
+			return $this->makeResult( $content );
+		}
+
+		private function extractTitles( Item $spine ): array {
+			// find titles
+			$matches   = [];
+			$ifMatched = preg_match_all( '/(<h[1-4].*>.*<\/h[1-4]>)+/', $spine->getData(), $matches );
+			$titles    = [];
+			if ( $ifMatched ) {
+				foreach ( $matches as $match ) {
+					foreach ( $match as $item ) {
+						$title  = [];
+						$titled = preg_match_all( '/\>(.+)<\/+/', $item, $title );
+						if ( $titled ) {
+							$titles[] = Str::remove( [ "/>", "</" ], $title[ 1 ][ 0 ] );
+						}
+					}
+				}
+			}
+
+			return $titles;
+		}
+
+		private function extractText( Item $spine, array $titles ): array {
+			$text = $spine->getContents();
+			$text = Str::replace( [ "\t\t\t\n", "\t\t\n", "\t\t\t", "\t" ], '', $text );
+			$text = preg_split( '/(\\n)+/', $text );
+			if ( is_array( $text ) ) {
+				foreach ( $text as $key => $item ) {
+					if ( Str::wordCount( $item ) == 0 ) {
+						unset( $text[ $key ] );
+						continue;
+					}
+					// wrap titles with title tag
+					if ( in_array( $item, $titles ) ) {
+						$text[ $key ] = "<h3>" . $item . "</h3>";
+					}
+				}
+			}
+
+			return $text;
+		}
+
+		private function makeResult( $content ): array {
 			$result    = [];
 			$paragraph = '';
 			foreach ( $content as $index => $pages ) {
